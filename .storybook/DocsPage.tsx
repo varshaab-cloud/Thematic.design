e207b3851b6029e655c6efbc6f1743f57fd8eb19
Varsha <sumant.nashik@gmail.com>
2026-05-26 02:25:11 +0530

    test: use built-in DocsPage to diagnose docs.page mechanism

--- .storybook/DocsPage.tsx
+++ .storybook/DocsPage.tsx
@@ -1,22 +1,2 @@
-import React from "react"
-import {
-  Title,
-  Subtitle,
-  Primary,
-  Controls,
-  Description,
-  Stories,
-} from "@storybook/addon-docs/blocks"
-
-export function DocsPage() {
-  return (
-    <>
-      <Title />
-      <Subtitle />
-      <Primary />
-      <Controls />
-      <Description of="meta" />
-      <Stories />
-    </>
-  )
-}
+// Re-export the built-in default DocsPage to test that docs.page mechanism works
+export { DocsPage } from "@storybook/addon-docs/blocks"
